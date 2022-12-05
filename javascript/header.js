ae8c8371a78c686f8c9698cf29933858c9eaa52d
sean-126 <sungsihoon@gmail.com>
2022-12-05 12:56:50 +0900

    헤더 로고 인터렉션

--- javascript/header.js
+++ javascript/header.js
@@ -1,21 +1,18 @@
 const heroHeightEl = document.querySelector('.section-1');
 const headerEl = document.querySelector('.header');
+const logoEl = document.querySelector('.logo a img')
 
 //hero 높이값
 console.log(heroHeightEl.offsetHeight);
 
-console.log(document.documentElement.scrollTop)
 
 
-
-
-const scrollYValue = window.addEventListener('scroll', () => {
-    console.log(window.scrollY)
-    return window.scrollY
+let headerChange = window.addEventListener('scroll', () => {
+    if(window.scrollY > heroHeightEl.offsetHeight) {
+        headerEl.style.backgroundColor = '#fff';
+        logoEl.src = './images/logo_codingx_kr.png';
+    }else {
+        headerEl.style.backgroundColor = '#101F41';
+        logoEl.src = './images/logo_codingx_kr_dark.png';
+    }
 })
-
-if(scrollYValue > heroHeightEl) {
-    headerEl.style.backgroundColor = '#fff';
-}else {
-    headerEl.style.backgroundColor = '#000';
-}
