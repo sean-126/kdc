8d6d20e3ee6f06f3a6217bffbf71216478336fa0
sean-126 <sungsihoon@gmail.com>
2022-12-06 09:15:53 +0900

    header menu color change interaction

--- javascript/header.js
+++ javascript/header.js
@@ -1,20 +1,23 @@
 const heroHeightEl = document.querySelector('.section-1');
 const headerEl = document.querySelector('.header');
 const logoEl = document.querySelector('.logo a img')
-const headerMenuEls = document.querySelector('.menu')
+const headerMenuEls = [...document.querySelectorAll('.menu > li')]
 
-//hero 높이값
-console.log(heroHeightEl.offsetHeight);
-
-
+console.log(typeof headerMenuEls)
 
 let headerChange = window.addEventListener('scroll', () => {
-    if(window.scrollY > heroHeightEl.offsetHeight) {
-        headerEl.style.backgroundColor = '#fff';
-        logoEl.src = './images/logo_codingx_kr.png';
-    }else {
-        headerEl.style.backgroundColor = '#101F41';
-        logoEl.src = './images/logo_codingx_kr_dark.png';
-        headerMenuEls.style.color = '#fff';
-    }
+  if (window.scrollY > heroHeightEl.offsetHeight) {
+    headerEl.style.backgroundColor = '#fff';
+    logoEl.src = './images/logo_codingx_kr.png';
+    headerMenuEls.forEach((el) => {
+      el.style.color = '#3F3F3F';
+    })
+  } else {
+    headerEl.style.backgroundColor = '#101F41';
+    logoEl.src = './images/logo_codingx_kr_dark.png';
+    headerMenuEls.forEach((el) => {
+      el.style.color = '#EBEBEB';
+    })
+  }
 })
+
