29989e3929fa89340db71dd6bd732adaa8d0953d
sean-126 <sungsihoon@gmail.com>
2022-12-07 12:46:18 +0900

    웹 헤더 수정 & FAQ br태그로 컨텐츠 최적화

--- javascript/header.js
+++ javascript/header.js
@@ -2,6 +2,7 @@ const heroHeightEl = document.querySelector('.section-1');
 const headerEl = document.querySelector('.header');
 const logoEl = document.querySelector('.logo a img')
 const headerMenuEls = [...document.querySelectorAll('.menu > li')]
+const headerApplyBtnEl = document.querySelector('.header .apply-btn a')
 
 console.log(typeof headerMenuEls)
 
@@ -9,12 +10,14 @@ let headerChange = window.addEventListener('scroll', () => {
   if (window.scrollY > heroHeightEl.offsetHeight) {
     headerEl.style.backgroundColor = '#fff';
     logoEl.src = './images/logo_codingx_kr.png';
+    headerApplyBtnEl.style.display = 'block';
     headerMenuEls.forEach((el) => {
       el.style.color = '#3F3F3F';
     })
   } else {
     headerEl.style.backgroundColor = '#101F41';
     logoEl.src = './images/logo_codingx_kr_dark.png';
+    headerApplyBtnEl.style.display = 'none';
     headerMenuEls.forEach((el) => {
       el.style.color = '#EBEBEB';
     })
